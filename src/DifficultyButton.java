f4c0a0dc8e2f9d70aaced8ea7c436b0cbfaabd73
Josephine Js <josephine.js33@gmail.com>
2014-05-26 16:55:40 +1000

    difficulty button edited

--- src/DifficultyButton.java
+++ src/DifficultyButton.java
@@ -1,48 +1,31 @@
+
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Insets;
+
 import java.awt.Point;
 
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 
 
+
 public class DifficultyButton extends StyledButton{
-	
-<<<<<<< HEAD
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	public DifficultyButton(JLabel difficultyLabel, Point p){
 
-	public DifficultyButton(String text, Point p, Insets inset, Dimension d){
-		this.setBounds(inset.left + p.x, inset.top + p.y, d.width, d.height );
 		this.setLocation(p);
-		this.setText(text);
-		
-		Font myFont = new Font("Monospaced", Font.PLAIN, 25);
-=======
-	public DifficultyButton(String text, Point p){
-		
-		this.setLocation(p);
-		this.setText(text);
+		//this.setText(text);
 
 		this.setSize(150, 90);	
-		Font myFont = new Font("AR Destine", Font.PLAIN, 25);
-				
->>>>>>> 396c916e77fb304b712e936a4bef72369985f13f
-		this.setFont(myFont);
-		this.setForeground(Color.black);
-		this.setBackground(Color.black);
-		this.setOpaque(false);
-		
-		
-		//ImageIcon icon = new ImageIcon(WALL_IMAGE); 
-		//JLabel thumb = new JLabel();
-		//thumb.setIcon(icon);
+		this.add(difficultyLabel);
+		//Font myFont = new Font("AR Destine", Font.PLAIN, 25);
 
+		//this.setFont(myFont);
+		//this.setBackground(Color.black);
+
+		//this.setForeground(Color.white);
 	}
-
 }
--- src/DifficultySelection.java
+++ src/DifficultySelection.java
@@ -1,11 +1,17 @@
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -24,16 +30,52 @@ public class DifficultySelection extends JPanel {
 	
 	private static final int xPositionButton = 225;
 	private static final int yPositionButton = 100;
+	private static final int xDifficultyLabel = 150;
+	private static final int yDifficultyLabel = 90;
 	private static final int vGap = 30;
 	
 	//public JFrame difficultySelection;
-	
+	private JLabel easyLabel;
+	private JLabel mediumLabel;
+	private JLabel hardLabel;
 	
 	public DifficultySelection(final JFrame mainFrame, final int mode, final int players, final JPanel prev){
 		
-		easyButton = new DifficultyButton("Easy", new Point(xPositionButton, yPositionButton));
-		mediumButton = new DifficultyButton("Medium", new Point(xPositionButton, (2 * yPositionButton + vGap)));
-		hardButton = new DifficultyButton("Hard", new Point(xPositionButton,3 * yPositionButton + (2 * vGap)));
+		File easyFile = new File("easy.png");
+		File mediumFile = new File("Medium.png");
+		File hardFile = new File("Hard.png");
+        Image easyImage = null;
+        Image mediumImage = null;
+        Image hardImage = null;
+        try {
+            easyImage = ImageIO.read(easyFile);
+            mediumImage = ImageIO.read(mediumFile);
+            hardImage = ImageIO.read(hardFile);
+        } catch (IOException e) {}
+        easyLabel = new JLabel();
+        easyLabel.setSize(new Dimension(xDifficultyLabel, yDifficultyLabel));
+        easyLabel.setLocation(new Point(100, 20));
+        Image scaledEasyButton = easyImage.getScaledInstance(easyLabel.getWidth(), easyLabel.getHeight(), Image.SCALE_SMOOTH);    
+        ImageIcon easyIcon = new ImageIcon(scaledEasyButton);
+        easyLabel.setIcon(easyIcon);
+        
+        mediumLabel = new JLabel();
+        mediumLabel.setSize(new Dimension(xDifficultyLabel, yDifficultyLabel));
+        mediumLabel.setLocation(new Point(100, 20));
+        Image scaledMediumButton = mediumImage.getScaledInstance(mediumLabel.getWidth(), mediumLabel.getHeight(), Image.SCALE_SMOOTH);    
+        ImageIcon mediumIcon = new ImageIcon(scaledMediumButton);
+        mediumLabel.setIcon(mediumIcon);
+        
+        hardLabel = new JLabel();
+        hardLabel.setSize(new Dimension(xDifficultyLabel, yDifficultyLabel));
+        hardLabel.setLocation(new Point(100, 20));
+        Image scaledHardButton = hardImage.getScaledInstance(hardLabel.getWidth(), hardLabel.getHeight(), Image.SCALE_SMOOTH);    
+        ImageIcon hardIcon = new ImageIcon(scaledHardButton);
+        hardLabel.setIcon(hardIcon);
+		
+		easyButton = new DifficultyButton(easyLabel, new Point(xPositionButton, yPositionButton));
+		mediumButton = new DifficultyButton(mediumLabel, new Point(xPositionButton, (2 * yPositionButton + vGap)));
+		hardButton = new DifficultyButton(hardLabel, new Point(xPositionButton,3 * yPositionButton + (2 * vGap)));
 		backButton = new BackButton(new Point(20, 480), prev, mainFrame, this);
 		current = this;
 		
